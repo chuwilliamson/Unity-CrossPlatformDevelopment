0ea126aa9acd67ff81f6f2d96a7874baa8755df4
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-05-15 14:52:40 -0500

    Working on defining what a character is

---
+++ Assets/ScriptableObject/Character.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "Characters/Default")]
+public class Character : ScriptableObject
+{
+    public int ID;
+    public string Name;
+    public Stats _Stats;
+
+    public virtual void OnEnable()
+    {
+        _Stats = Instantiate(_Stats);
+    }
+}
