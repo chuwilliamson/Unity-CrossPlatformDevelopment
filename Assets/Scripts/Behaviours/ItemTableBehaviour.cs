7b28c057fd74e60d7f1184f2c002911896ea2c40
Matthew Williamson <chuwilliamson@gmail.com>
2017-05-18 10:07:15 -0500

    add the global blackboard scriptable, add the characterbehaviour example

--- Assets/Scripts/Behaviours/ItemTableBehaviour.cs
+++ Assets/Scripts/Behaviours/ItemTableBehaviour.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
+
 using UnityEngine;
 
 public class ItemTableBehaviour : MonoBehaviour
---
+++ Assets/Scripts/Behaviours/ZambinoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZambinoBehaviour : CharacterBehaviour
+{
+    public GameObject PlayerGameObject;
+    public Rigidbody2D rb;
+    public Sensor sensor;
+    private void Start()
+    {
+        PlayerGameObject = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+    }
+    [InspectorReadOnly]
+    float distance;
+
+    
+    [Range(0, 10)]
+    public float range;
+    private void Update()
+    {
+        distance = Vector3.Distance(transform.position, PlayerGameObject.transform.position);
+        if (Vector3.Distance(transform.position, PlayerGameObject.transform.position) < range)
+        {
+            bool behind = Vector3.Dot(transform.right, PlayerGameObject.transform.right) >= .9;
+            if (behind)
+                rb.AddForce(transform.right * 1f, ForceMode2D.Impulse);
+            else
+                rb.AddForce(transform.right * 1f, ForceMode2D.Impulse);
+        }
+            
+    }
+}
